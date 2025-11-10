2ed5126fd0cb1d6fecb8665327705ffbe637d5e6
Ed <lobas.reserve@gmail.com>
2025-11-10 13:23:10 +0200

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'app.lovable.34d0c9e38c8046c798ca97d39bc65981',
-  appName: 'scoutflow-app',
+  appId: 'com.lovable.scoutflow',
+appName: 'ScoutFlow',
   webDir: 'dist',
   server: {
     url: 'https://34d0c9e3-8c80-46c7-98ca-97d39bc65981.lovableproject.com?forceHideBadge=true',
